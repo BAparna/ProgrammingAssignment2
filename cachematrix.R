67b058bba3de162eda3b62a6ea05d4a5342b97a7
AparnaB <aparnabulusu79@gmail.com>
2014-07-27 20:27:32 +0530

    first try

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
+## this function will create a matrix
 ## functions do
 
-## Write a short comment describing this function
+## Used to create a special cachable matrix Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  x<-NULL
+  
+  x<<-matrix(data,nrow,ncol)
+  x
+  
 }
 
 
-## Write a short comment describing this function
+## This function cheks if a cached matrix is already present and creates its inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m<-x
+  if(!is.null(m))
+  {
+    message( "Getting cached matrix")
+    return(m)
+    
+  }
+  m<-x()
 }
